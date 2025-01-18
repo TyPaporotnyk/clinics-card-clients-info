60df8ebed7238389deb0e159bdaae9a052ad59f7
TyPaporotnyk <daniil.svadkovsky@gmail.com>
2025-01-18 16:53:00 +0200

    Created decorator for gspread exception catching

--- app/excel.py
+++ app/excel.py
@@ -3,6 +3,8 @@ import time
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+from app.utils import retry_request
+
 
 class GoogleSheetsClient:
     def __init__(self, google_sheets_key: str, worksheet_name: str, token_path: str):
@@ -22,31 +24,36 @@ class GoogleSheetsClient:
         credentials = ServiceAccountCredentials.from_json_keyfile_name(self.token_path, scope)
         return gspread.authorize(credentials)
 
+    @retry_request()
     def write_row(self, row):
         all_values = self.sheet.col_values(3)
         last_row_index = len(all_values)
-
         self.sheet.insert_row(row, last_row_index + 1)
         time.sleep(1)
 
+    @retry_request()
     def get_column_values(self) -> list[str]:
         value = self.sheet.col_values(1)
         time.sleep(1)
         return value
 
+    @retry_request()
     def insert_element_at(self, row: int, col: int, value):
         self.sheet.update_cell(row, col, value)
         time.sleep(1)
 
+    @retry_request()
     def get_element_at(self, col: int, row: int):
         value = self.sheet.cell(row, col).value
         time.sleep(1)
         return value
 
+    @retry_request()
     def update_element_at(self, col: int, row: int, value):
         self.sheet.update_cell(row, col, f"{value}")
         time.sleep(1)
 
+    @retry_request()
     def find(self, value: str) -> tuple[int, int]:
         cell = self.sheet.find(value)
         time.sleep(1)
@@ -56,6 +63,7 @@ class GoogleSheetsClient:
         else:
             raise ValueError(f"Value '{value}' not found in the sheet")
 
+    @retry_request()
     def find_last(self, value: str):
         cells = self.sheet.findall(value)
         time.sleep(1)
@@ -66,10 +74,12 @@ class GoogleSheetsClient:
         else:
             raise ValueError(f"Value '{value}' not found in the sheet")
 
+    @retry_request()
     def insert_row_at(self, row_index: int, values: list):
         self.sheet.insert_row(values, row_index)
         time.sleep(1)
 
+    @retry_request()
     def insert_column_at(self, col_index: int, values: list):
         self.sheet.insert_col(values, col_index)
         time.sleep(1)
---
+++ app/utils.py
@@ -0,0 +1,22 @@
+import time
+from gspread.exceptions import APIError
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def retry_request(retries=5, delay=10):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for attempt in range(retries):
+                try:
+                    return func(*args, **kwargs)
+                except APIError as e:
+                    if e.response.status_code in [500, 503]:
+                        logger.warning("Retrying... (%s) wait for %s secconds", {attempt + 1}/{retries}, delay)
+                        time.sleep(delay)
+                    else:
+                        raise 
+            raise Exception("Max retries exceeded")
+        return wrapper
+    return decorator
